a33b0bd23dd6d8bb03df94fbf1450bb36efdcbda
joelsel <joel.selvaretnam@gmail.com>
2022-06-27 07:51:01 +0800

    working on implementing classes

--- scratchpad.py
+++ scratchpad.py
@@ -1,3 +1,5 @@
+"""
+
 class experiment:
     def __init__(self, day, date):
         self.day = day
@@ -8,3 +10,20 @@ class experiment:
         print (prompt)
 z = experiment("monday", "23")
 z.output()
+"""
+class foo:
+    def __init__(self, time, day, date):
+        self.time = time
+        self.day = day
+        self.date = date
+
+    def thing(self):
+        output = ()
+        input("time is: ", self.time)
+        input("day is: ", self.day)
+        input("date is: ", self.date)
+        #print (output)
+
+foo.thing("empty", "empty", "empty")
+
+#working on implementing classes
