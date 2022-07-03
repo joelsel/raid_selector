95e8001a9c2b569db05eb9974b0e9b17d5292dce
Joel-Aftershock <joel.selvaretnam@gmail.com>
2022-07-03 22:58:17 +0800

    updated functional input in class test

--- scratchpad.py
+++ scratchpad.py
@@ -1,29 +1,38 @@
 """
 
 class experiment:
-    def __init__(self, day, date):
+    def __init__(self, day, month):
         self.day = day
-        self.date = date
+        self.month = month
 
     def output(self):
-        prompt = ("today is {} the {}".format(self.day,self.date))
+        prompt = ("today is {} the {}".format(self.day,self.month))
         print (prompt)
 z = experiment("monday", "23")
 z.output()
 """
+
 class foo:
-    def __init__(self, time, day, date):
+    def __init__(self, time, day, month):
         self.time = time
         self.day = day
-        self.date = date
+        self.month = month
+
+
+    def testing(self):
+        #f"time is {self.time}, day is {self.day}, month is {self.month}"
+        prompt = ("prompt time is {}, prompt day is {}, prompt month is {}".format(self.time, self.day, self.month))
+        name = input ("name = ")
+        print (prompt)
+        print (f"name was {name}")
 
     def thing(self):
-        output = ()
-        input("time is: ", self.time)
-        input("day is: ", self.day)
-        input("date is: ", self.date)
-        #print (output)
+        self.time = input("time is: ")
+        self.day = input("day is: ")
+        self.month = input("month is: ")
+        print (f"\ninput time was {self.time},\ninput day was {self.day},\ninput month was {self.month}\n")
 
-foo.thing("empty", "empty", "empty")
-
+bar = foo("testtime", "testday", "testmonth")
+#bar.testing()
+bar.thing()
 #working on implementing classes
