e4a9424d1ece93130ffefe6847d61c713e942961
Joel@Mac <joel.selvaretnam@gmail.com>
2022-07-18 16:53:44 +0800

    added roadmap, cleanup comments from raidSelector

--- raidSelector.py
+++ raidSelector.py
@@ -11,17 +11,7 @@ def timestamp(now):
     print("getTime check: {}".format(getTime))
     f.close()
 
-#terminus = ("\n{:-^60}\n".format(" end of output "))
 prog_init = 1
-# cycle_count = ()
-# spec_sample = []
-
-# tank_wt, healer_wt, melee_wt, ranged_wt = [0, 1, 0, 1]
-# cnt_tank, cnt_healer, cnt_melee, cnt_ranged = [0, 0, 0, 0]
-
-# class_list = ["Mage", "Druid", "Warrior", "Paladin", "Priest", "Shaman", "Rogue", "Hunter", "Warlock",
-#               "Demon Hunter", "Death Knight", "Monk"]
-# role_list = ['Tank', 'Healer', 'Melee', 'Ranged']
 
 # RaidSelector - defining default values
 class_list = ["Mage", "Druid", "Warrior", "Paladin",
@@ -56,26 +46,6 @@ def raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged):
     f.close()
 
 
-#todo find out how to return which item number an element of a list is, that matches x criteria
-
-# role_sample = random.choices(role_list, weights=role_wt)
-
-# class_sample = random.choices(class_list)
-
-#todo strategy > check valid roles > pick role > check valid classes > pick class
-
-#todo set maxvalue for class valid selection
-
-#def valid_class():
-
-
-
-# todo class weights based on role
-
-# weight_test = [tank_wt, healer_wt, melee_wt, ranged_wt]
-# role_wt = weight_test
-
-# TODO ADD ROLECHECK FUNCTION
 def sampleRole(role_wt):
     global role_sample
     role_sample = random.choices(role_list, weights=role_wt)
@@ -253,8 +223,6 @@ def monk_spec():
 
 
 
-
-
 # class picker redirection
 def class_redirect(class_sample):
     if "Mage" in class_sample:
@@ -285,6 +253,7 @@ def class_redirect(class_sample):
         print("class_picker error")
 
     role_counter()
+
 """
 #class_redirect debugging
     print("".join(class_sample))
@@ -307,48 +276,34 @@ def class_picker_main(class_sample, role_sample, spec_sample, cyc):
     out_class = ("".join(class_sample))
     out_role = ("".join(role_sample))
     out_spec = ("".join(spec_sample))
-    # TODO fix append statement
     line_output.append("Member {:03}: {} {:<10} {:<15} {:<10}\n".format((cyc + 1), " >> ", out_role, out_class, out_spec))
-    # out_class = ("null")
-    # out_role = ("null")
-    # out_spec = ("null")
  
 # development file write
 def write_file(line_output,cycle_count):
     while True:
             try:
-                # print("Selection is ", run_choice, " executing Class Picker.")
-                #write_mode = int(input("1 for Append, 0 for overwrite: "))
                 if write_mode == 0:
                     with open("hello.txt", "a") as f:
                         print("selection is: ", write_mode, " overwrite mode active.")
-                        # TODO inelegant but works, next to remove join from line output statement
                         f.write("".join(line_output))
-                        #print(terminus)
                         print("\n{} line(s) output to file.\n".format(cycle_count))
                         f.write("\n{} line(s) output to file.\n".format(cycle_count))
-                        #f.write("".join(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged)))
-                        #f.write(terminus)
-                        #f.write(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged))
-                        #print(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged))
-
                     break
+
                 elif write_mode == 1:
                     with open("hello.txt", "a") as f:
                         print("selection is: ", write_mode, " append mode active.")
                         f.write("".join(line_output))
-                        #print(terminus)
                         print("\n{} line(s) output to file.\n".format(cycle_count))
                         f.write("\n{} line(s) output to file.\n".format(cycle_count))
-                        #f.write("".join(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged)))
-                        #f.write(terminus)
-                        #f.write(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged))
-                        #print(raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged))
+
                 elif run_choice == 0:
                     print("Under Construction, please try again.")
+
             except ValueError:
                 print("E-03: Invalid input, returning to main menu")
                 continue
+
             break
             # else:
             #     print("E-04: Input not recognized")
@@ -359,11 +314,13 @@ def write_file(line_output,cycle_count):
 def class_picker_v2():
     global cycle_count
     cycle_count = int(input("Number of cycles, 0 to exit: "))
-    #try:
+
     if cycle_count < 0:
         print(" try again , value cannot be less than 0")
+
     elif cycle_count == 0:
         print(" Thank you")
+
     elif cycle_count > 0:
         for cyc in range(cycle_count):
             role_check(cnt_tank, cnt_healer, cnt_melee, cnt_ranged)
@@ -371,101 +328,70 @@ def class_picker_v2():
             class_check(role_sample)
             sampleClass()
             class_redirect(class_sample)
-            class_picker_main(class_sample, role_sample, spec_sample, cyc) # needs ID, role, class, spec variables passed to write_file
-        #except: ValueError
-
-        #debug line_output
-        #print (line_output)
-
-        write_file(line_output, cycle_count) # write_file not currently working
-        print("Value error")
+            class_picker_main(class_sample, role_sample, spec_sample, cyc)
+       
+        write_file(line_output, cycle_count) 
 
     print("End of class_picker\n")
-    #print (line_output)
-    # TODO integrate class_picker_main to development block
 
-
-# todo add padding zeroes to member -- done
-# todo add write selection mode to output file
-# todo cleanup slice selection
-# todo assign correct spec to class role
-
-#print("\nclass_list length is: ", len(class_list))
-
-#print("Role length is", len(role_list), "\n")
 
 while prog_init == 1:
         
     try:
         run_choice = int(input("Main Menu: Press 1 for class_dev, Press 2 for Class Picker v2: "))
         print("selection is going to be: ", run_choice)
+
     except ValueError:
         print("E-01: Invalid input / no input, please try again.")
         continue
+
     if run_choice < 0:
         print("E-02: Choice must be greater than 0, please try again.")
         continue
+
     elif run_choice == 1:
         print("Selection is: ", run_choice, " executing class_redirect.")
         class_redirect()
+
     elif run_choice == 2:
         while run_choice == 2:
             try:
                 print("Selection is ", run_choice, " executing Class Picker.")
                 write_mode = int(input("1 for Append, 0 for overwrite: "))
+
                 if write_mode == 0:
-                    # todo add while/break for write mode exception
-                    # todo set exit code state instead of while/break??
                     timestamp(now)
                     with open("hello.txt", "a") as f:
                         print("selection is: ", write_mode, " overwrite mode active.")
                         f.write("\n")
-                        #timestamp(now)
-                        #f.write(("timestamp: {:-^60}\n").format(now))
-                        #print(("timestamp: {:-^60}\n").format(now))
                         class_picker_v2()
                         raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged)
-                        #print(terminus)
-                        #print("\n{:-^60}\n " .format(" end of output "))
-                        #f.write(("\n{:-^60}\n " .format(" end of output ")))
                         f.write(terminus)
                     break
+
                 elif write_mode == 1:
                     timestamp(now)
                     with open("hello.txt", "a") as f:
                         print("selection is: ", write_mode, " append mode active.")
-                        #timestamp(now)
-                        #f.write(("timestamp: {:-^60}\n").format(now))
-                        #print(("timestamp: {:-^60}\n").format(now))
                         class_picker_v2()
                         raidReport(cnt_tank, cnt_healer, cnt_melee, cnt_ranged)
-                        #print(terminus)
-                        #print("\n{:-^60}\n " .format(" end of output "))
-                        #f.write(("\n{:-^60}\n " .format(" end of output ")))
                         f.write(terminus)
+
             except ValueError:
                 print("E-03: Invalid input, returning to main menu")
                 continue
             break
+
     elif run_choice == 0:
         print("Under Construction, please try again.")
-            # else:
-            #     print("E-04: Input not recognized")
-            #     continue
-    # print("outer loop end marker")
     break
-# print("mark3")
-# todo >> continue is part of the while loop, not the try loop. create a new while loop to have continue return to expected location
 
-#TODO replace class_picker with class_picker_main
-#print(terminus)
 print("\nend of program")
 
-#pass the necessary variables to your function!!!!!!!
-
-#terminus now activates from within write fiel
 
 #TODO note: print while file is open writes to file
 #TODO 20220616 'lines output to file' being overwritten
 #TODO 20220616 formatting issues
 #TODO 20220617 research __init__, classes , self method
+#TODO 20220718 check exception catcher in write_file, pruning comments from main file, exception catcher for all input statements
+#TODO 20220718 rework RaidReport file open style to 'with open as f'
